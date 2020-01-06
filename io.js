eee8acd99b73ff2a2a6b8f4e4e6cfcfcacaa8ca7
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-01-06 16:00:17 -0600

    Circle Game complete

---
+++ io.js
@@ -0,0 +1,16 @@
+// io.js server created
+var io = require('socket.io')();
+
+// Listen for new connections from clients (socket)
+io.on('connection', function (socket) {
+    console.log('Client connected to socket.io!');
+    //new code below
+    socket.on('add-circle', function (data) {
+        io.emit('add-circle', data);
+    });
+});
+
+
+// io represents socket.io on the server - let's export it
+module.exports = io;
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+To sync your code with the latest commit:
+
+git fetch --all
+
+git reset --hard origin/master
+
